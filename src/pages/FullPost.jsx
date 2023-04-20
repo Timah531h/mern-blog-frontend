bafb480ed75a2d2ed9fa51f312877cb7e997bd4e
Timah531h <kovzhun03@gmail.com>
2023-04-20 09:54:53 +0300

    1
    front

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -9,33 +9,29 @@ export const FullPost = () => {
     <>
       <Post
         id={1}
-        title="Roast the code #1 | Rock Paper Scissors"
+        title="Тестовый фронт"
         imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
         user={{
           avatarUrl:
             "https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png",
-          fullName: "Keff",
+          fullName: "Тимофей Ковжун",
         }}
-        createdAt={"12 июня 2022 г."}
+        createdAt={"04 апреля 2023 г."}
         viewsCount={150}
         commentsCount={3}
-        tags={["react", "fun", "typescript"]}
+        tags={["Пост", "Лента постов", "!"]}
         isFullPost
       >
         <p>
-          Hey there! 👋 I'm starting a new series called "Roast the Code", where
-          I will share some code, and let YOU roast and improve it. There's not
-          much more to it, just be polite and constructive, this is an exercise
-          so we can all learn together. Now then, head over to the repo and
-          roast as hard as you can!!
+          Тут будут отображаться новые статьи
         </p>
       </Post>
       <CommentsBlock
         items={[
           {
             user: {
-              fullName: "Вася Пупкин",
-              avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
+              fullName: "Игорь шаровский",
+              avatarUrl: "https://res.cloudinary.com/ddshgdehu/image/upload/v1680700103/162144132918582960_zghzwt.jpg",
             },
             text: "Это тестовый комментарий 555555",
           },
@@ -44,7 +40,7 @@ export const FullPost = () => {
               fullName: "Иван Иванов",
               avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
             },
-            text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
+            text: "Когда новый пост",
           },
         ]}
         isLoading={false}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,38 +19,38 @@ export const Home = () => {
           {[...Array(5)].map(() => (
             <Post
               id={1}
-              title="Roast the code #1 | Rock Paper Scissors"
-              imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+              title="Тест Фронта"
+              imageUrl="https://res.cloudinary.com/ddshgdehu/image/upload/c_scale,h_328/v1680699491/123123_j69puu.jpg"
               user={{
                 avatarUrl:
-                  'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
-                fullName: 'Keff',
+                  'https://res.cloudinary.com/ddshgdehu/image/upload/v1680699772/-mzYwM2dRI8_ysewbb.jpg',
+                fullName: 'Тимофей Ковжун',
               }}
-              createdAt={'12 июня 2022 г.'}
+              createdAt={'03 апреля 2023 г.'}
               viewsCount={150}
               commentsCount={3}
-              tags={['react', 'fun', 'typescript']}
+              tags={['ТРиТПО', 'Тест', 'ТРиТПО']}
               isEditable
             />
           ))}
         </Grid>
         <Grid xs={4} item>
-          <TagsBlock items={['react', 'typescript', 'заметки']} isLoading={false} />
+          <TagsBlock items={['Tritpo', 'Aksis', 'AVP']} isLoading={false} />
           <CommentsBlock
             items={[
               {
                 user: {
-                  fullName: 'Вася Пупкин',
-                  avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
+                  fullName: 'Тимофей Ковжун',
+                  avatarUrl: 'https://res.cloudinary.com/ddshgdehu/image/upload/v1680699772/-mzYwM2dRI8_ysewbb.jpg',
                 },
-                text: 'Это тестовый комментарий',
+                text: 'Это крутой блог',
               },
               {
                 user: {
-                  fullName: 'Иван Иванов',
-                  avatarUrl: 'https://mui.com/static/images/avatar/2.jpg',
+                  fullName: 'Игорь Шаровский',
+                  avatarUrl: 'https://res.cloudinary.com/ddshgdehu/image/upload/v1680700103/162144132918582960_zghzwt.jpg',
                 },
-                text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
+                text: 'Всё четко!!!',
               },
             ]}
             isLoading={false}
